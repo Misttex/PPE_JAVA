999313e843e638741ac2dd0b658fa5a43f606920
Quentin <magalhaes.quentin@free.fr>
2021-01-21 12:05:15 +0100

    Formulaire de connexion

---
+++ src/main/java/demo/com/example/demo/connexion/Connexion.java
@@ -0,0 +1,18 @@
+package demo.com.example.demo.connexion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Connexion implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("acceuil");
+        registry.addViewController("/acceuil").setViewName("acceuil");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/fichefrais").setViewName("fichefrais");
+    }
+
+}
